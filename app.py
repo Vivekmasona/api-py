b6630702ec49782671ee7d94f3f35e9fba978abb
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-12-08 10:28:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,19 @@
-from typing import Optional
-import httpx
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-from models import GithubUserModel
+from pytube import YouTube
+from flask import Flask, session, url_for, send_file, render_template, redirect, request
+from io import BytesIO
 
-app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "my_secret_key"
 
-limits = httpx.Limits(max_keepalive_connections=5, max_connections=10)
-timeout = httpx.Timeout(timeout=5.0, read=15.0)
-client = httpx.AsyncClient(limits=limits, timeout=timeout)
+@app.route("/", methods=["POST", "GET"])
+def index():
+    if request.method == "POST":
+        session["link"] = request.form.get("url")
+        url = YouTube(session["link"])
+        url.check_availability()
+        return render_template("download.html", url=url)
 
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    print("shutting down...")
-    await client.aclose()
-
-
-@app.get("/", response_class=HTMLResponse)
-async def index(request: Request, username: str = None):
-    if not username:
-        return templates.TemplateResponse("index.html", context={"request": request})
-
-    user = await get_github_profile(request, username)
-    if not user:
-        return templates.TemplateResponse("404.html", context={"request": request})
-
-    return templates.TemplateResponse("index.html", context={"request": request, "user": user})
+    return render_template('index.html')
 
 @app.route("/audio/<video_id>", methods=["GET"])
 def audio(video_id):
@@ -59,16 +44,9 @@ def download():
 
         return send_file(buffer, as_attachment=True, download_name=video.title, mimetype=video.mime_type)
 
+    return "Invalid or missing YouTube link parameter."
 
-@app.get("/{username}", response_model=GithubUserModel)
-async def get_github_profile(request: Request, username: str) -> Optional[GithubUserModel]:
-    headers = {"accept": "application/vnd.github.v3+json"}
+if __name__ == '__main__':
+    app.run(debug=True)
 
-    response = await client.get(f"https://api.github.com/users/{username}", headers=headers)
 
-    if response.status_code == 404:
-        return None
-
-    user = GithubUserModel(**response.json())
-
-    return user
