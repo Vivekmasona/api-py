98672fe77fec2804e667bd27195855d0e7cbf057
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-12-08 10:25:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,35 @@ async def index(request: Request, username: str = None):
 
     return templates.TemplateResponse("index.html", context={"request": request, "user": user})
 
+@app.route("/audio/<video_id>", methods=["GET"])
+def audio(video_id):
+    youtube_link = f"https://www.youtube.com/watch?v={video_id}"
+    buffer = BytesIO()
+    url = YouTube(youtube_link)
+    audio_stream = url.streams.filter(only_audio=True).first()
+
+    if audio_stream:
+        audio_stream.stream_to_buffer(buffer)
+        buffer.seek(0)
+
+        return send_file(buffer, as_attachment=True, download_name=f"{url.title}.mp3", mimetype="audio/mpeg")
+
+    return "Invalid or missing YouTube link parameter."
+
+@app.route("/video", methods=["GET"])
+def download():
+    youtube_link = request.args.get("url")
+    if youtube_link:
+        buffer = BytesIO()
+        url = YouTube(youtube_link)
+        video = url.streams.get_highest_resolution()
+
+        # Stream the video to the buffer
+        video.stream_to_buffer(buffer)
+        buffer.seek(0)
+
+        return send_file(buffer, as_attachment=True, download_name=video.title, mimetype=video.mime_type)
+
 
 @app.get("/{username}", response_model=GithubUserModel)
 async def get_github_profile(request: Request, username: str) -> Optional[GithubUserModel]:
